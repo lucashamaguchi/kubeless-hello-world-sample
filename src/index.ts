721b4f35b3d83905b8c6d697791a184228e10342
lucashamaguchi <hamaguchi.lucas@gmail.com>
2020-11-16 18:20:15 -0300

    log env to test if deploy is inject env vars

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ require("dotenv").config();
 
 export async function start(event, context) {
   console.log("fala ai 2")
-  console.log(event)
-  console.log(context)
+  console.log("data: ", event.data)
+  console.log(JSON.stringify(process.env))
 }
